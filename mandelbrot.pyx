9461cf5dc520d8ed1af363f1dab76db97fa89d78
Thouis (Ray) Jones <thouis@gmail.com>
2015-09-30 09:03:27 -0400

    add python version for reference

--- mandelbrot.pyx
+++ mandelbrot.pyx
@@ -24,3 +24,17 @@ cpdef mandelbrot(np.complex64_t [:, :] in_coords,
                         break
                     z = z * z + c
                 out_counts[i, j] = iter
+
+
+def mandelbrot_python(in_coords,
+                      out_counts,
+                      max_iterations=511):
+    for i in range(in_coords.shape[0]):
+        for j in range(in_coords.shape[1]):
+            c = in_coords[i, j]
+            z = 0
+            for iter in range(max_iterations):
+                if magnitude_squared(z) > 4:
+                    break
+                z = z * z + c
+            out_counts[i, j] = iter
