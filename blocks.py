c62cf154c71eee16b0fa858d78161d1dee336117
Thouis (Ray) Jones <thouis@gmail.com>
2015-09-29 20:54:09 -0400

    multiprocessing block and row versions

--- blocks.py
+++ blocks.py
@@ -2,10 +2,24 @@ import numpy as np
 
 def by_blocks(array, num):
     '''
-    Generator that iterates over blocks in an array with (num x num) total blocks.
+    Generator that iterates over blocks in an array with (num x num) total
+    blocks.
+
     Resulting subarrays are views on the original array (shared data).
     '''
-
     for hblock in np.array_split(array, num, axis=0):
         for vblock in np.array_split(hblock, num, axis=1):
             yield vblock
+
+def by_block_slices(array, num):
+    '''
+    Generator that iterates over blocks in an array with (num x num) total
+    blocks, but returns slice indices rather than actual subarray.
+
+    '''
+    i_step = array.shape[0] / num
+    j_step = array.shape[1] / num
+    for i_lo in range(0, array.shape[0], i_step):
+        for j_lo in range(0, array.shape[1], j_step):
+            yield (slice(i_lo, i_lo + i_step),
+                   slice(j_lo, j_lo + j_step))
---
+++ mandelbrot_multi_block.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+import pyximport
+pyximport.install(setup_args={"include_dirs": [np.get_include()]},)
+import mandelbrot
+
+import pylab
+import time
+from blocks import by_block_slices
+import multiprocessing
+from multiprocessing import sharedctypes
+
+center = -0.575 - 0.575j
+width = 0.0025
+
+x = np.linspace(start=(-width / 2), stop=(width / 2), num=2000)
+xx = center + (x + 1j * x[:, np.newaxis]).astype(np.complex64)
+out_counts = np.zeros_like(xx, dtype=np.uint32)
+
+print "preview"
+
+# create shared arrays for xx and out_counts
+xx_shared = sharedctypes.RawArray('B', xx.view(np.uint8).size)
+out_counts_shared = multiprocessing.RawArray('B', out_counts.view(np.uint8).size)
+np.ndarray(buffer=xx_shared,
+           dtype=np.complex64,
+           shape=xx.shape)[...] = xx
+
+def wrap_mandelbrot(ij_slice, iterations=1024):
+    # wrap xx_shared, out_counts_shared in numpy wrapper
+    # these operations are fast
+    in_coords = np.ndarray(buffer=xx_shared,
+                           dtype=np.complex64,
+                           shape=xx.shape)
+    out_cnts = np.ndarray(buffer=out_counts_shared,
+                          dtype=np.uint32,
+                          shape=xx.shape)
+    mandelbrot.mandelbrot(in_coords[ij_slice], out_cnts[ij_slice], iterations)
+    return True
+
+pool = multiprocessing.Pool(4)
+
+start = time.time()
+slices = by_block_slices(xx, 100)
+pool.map(wrap_mandelbrot, slices)
+seconds = time.time() - start
+
+out_counts = np.ndarray(buffer=out_counts_shared,
+                        dtype=np.uint32,
+                        shape=xx.shape)
+
+print("{} seconds, {} million Complex FMAs / second".format(seconds, (out_counts.sum() / seconds) / 1e6))
+print("{} Million Complex FMAs".format(out_counts.sum() / 1e6))
+
+pylab.imshow(np.log(out_counts))
+pylab.show()
