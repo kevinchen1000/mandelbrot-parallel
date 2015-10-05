87bdd99e19143835d454c7c426549c994a1897f3
Thouis (Ray) Jones <thouis@gmail.com>
2015-10-05 08:55:47 -0400

    make threading and multiproc similar

--- mandelbrot_threading_row.py
+++ mandelbrot_threading_row.py
@@ -12,14 +12,14 @@ if __name__ == '__main__':
     in_coords, out_counts = make_coords()
 
     # this has to be here for scoping reasons
-    def wrap_mandelbrot(row, iterations=1024):
-        mandelbrot.mandelbrot(in_coords[row:(row + 1), :], out_counts[row:(row + 1), :], iterations)
+    def wrap_mandelbrot(ij_slice, iterations=1024):
+        mandelbrot.mandelbrot(in_coords[ij_slice], out_counts[ij_slice], iterations)
 
     pool = ThreadPool(4)
 
     with Timer() as t:
-        rows = range(in_coords.shape[0])
-        pool.map(wrap_mandelbrot, rows)
+        slices = by_rows(in_coords)
+        pool.map(wrap_mandelbrot, slices)
     seconds = t.interval
 
     print("{} seconds, {} million Complex FMAs / second".format(seconds, (out_counts.sum() / seconds) / 1e6))
